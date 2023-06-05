fce7174b23d1b9024b33a74adc39c9aad1069725
JoseFernandes975 <zehenrique752005@gmail.com>
2023-06-05 15:46:02 -0300

    Initial setup

---
+++ backend/src/main/java/com/josefernandes/jfdeliver/entities/Order.java
@@ -0,0 +1,17 @@
+package com.josefernandes.jfdeliver.entities;
+
+import java.time.Instant;
+
+import java.time.Instant;
+
+public class Order {
+	
+	private Long id;
+	private String address;
+	private Double latitude;
+	private Double longitude;
+	private Instant moment;
+	//private OrderStatus status;
+	private Double total;
+
+}
