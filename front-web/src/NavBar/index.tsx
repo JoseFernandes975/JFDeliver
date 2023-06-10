e567093c76064b19fdec6a115f3878133c737c1c
JoseFernandes975 <zehenrique752005@gmail.com>
2023-06-10 00:58:56 -0300

    Add NavBar

---
+++ front-web/src/NavBar/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css"
+import { ReactComponent as Logo } from "./logo.svg";
+
+function NavBar(){
+    return(
+        <nav className="main-navBar">
+          <Logo />
+          <a href="home"  className="logo-text">JF Delivery</a>
+        </nav>
+    )
+}
+
+export default NavBar;
